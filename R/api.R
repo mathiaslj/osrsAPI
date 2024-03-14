ca4d096c742a14cb031ccdce1176d182d3a5150e
mathiaslj <Math964b@yahoo.dk>
2024-03-14 20:34:53 +0100

    docs: Added documentation for functions

--- R/api.R
+++ R/api.R
@@ -1,10 +1,24 @@
-library(httr)
-library(xml2)
-
 ###################
 # Using httr
 
-osrs_api <- function(item = "Abyssal_whip", history = "latest", user_agent = "GE_price_tracker") {
+#' Get information about Grand Exchange prices of items in OSRS
+#'
+#' @description
+#' Use the Weird Gloop API to fetch information about Grand Exchange prices in
+#' Oldschool Runescape
+#'
+#' @param item `character` name of the item
+#' @param history `character` with type of request. Default is "latest", but "all" is
+#' also available
+#' @param user_agent `character` of the user agent
+#'
+#' @return A list of class [osrs_api]
+#'
+#' @export
+#'
+#' @examples
+#' osrs_api(item = "Abyssal_whip", history = "latest")
+osrs_api <- function(item, history = "latest", user_agent = "GE_price_tracker") {
 
   # Give valid values of history argument
   history_valid <- c("latest", "all")
@@ -14,27 +28,27 @@ osrs_api <- function(item = "Abyssal_whip", history = "latest", user_agent = "GE
   item_path <- paste0("/exchange/history/osrs/", history, "?name=", item)
 
   # Create the full URL
-  url <- modify_url("https://api.weirdgloop.org", path = item_path)
+  url <- httr::modify_url("https://api.weirdgloop.org", path = item_path)
 
   # GET the information from the URL
-  resp <- GET(url,
+  resp <- httr::GET(url,
               httr::user_agent(user_agent))
 
   # Check if success
-  if (http_status(resp)$category != "Success") {
+  if (httr::http_status(resp)$category != "Success") {
     stop("Failed to retrieve data from the API")
   }
 
   # Check if the format is json
-  if (http_type(resp) != "application/json") {
+  if (httr::http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
 
   # Parse the json, creating a list
-  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
+  parsed <- jsonlite::fromJSON(httr::content(resp, "text"), simplifyVector = FALSE)
 
   # If no success, stop and give the error message
-  if (!parsed$success) {
+  if (isFALSE(parsed$success)) {
     stop(parsed$error)
   }
 
@@ -62,12 +76,21 @@ osrs_api <- function(item = "Abyssal_whip", history = "latest", user_agent = "GE
   )
 }
 
+#' Print method for class
+#'
+#' @param x object of class `osrs_api`
+#' @param ... additional arguments passed to methods
+#'
+#' @export
+#'
+#' @examples
+#' whip <- osrs_api(item = "Abyssal_whip", history = "latest")
+#' print(whip)
 print.osrs_api <- function(x, ...) {
   # "condense" the information about response a bit, otherwise the str call goes nuts
   x$response <- lapply(x$response, class)
 
   cat("<OSRS Weird Gloop API ", x$path, ">\n", sep = "")
-  str(x)
+  utils::str(x)
+  invisible(x)
 }
-
-test <- osrs_api(item = "bob", history = "latest")
