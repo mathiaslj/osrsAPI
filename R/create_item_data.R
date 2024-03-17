df23c5418a9dc43772e6005b4197d2f01b7231f1
mathiaslj <Math964b@yahoo.dk>
2024-03-17 23:15:38 +0100

    bugfix: Swapped entries in grepl call. Also added package prefixes to dplyr functions

--- R/create_item_data.R
+++ R/create_item_data.R
@@ -25,16 +25,16 @@ create_item_data <- function(.x) {
     dplyr::bind_rows(.id = "item")
 
   # If we have not gotten just a sample, give the relative day of the price
-  if (!grepl(.x$path, "sample")) {
+  if (!grepl("sample", .x$path)) {
     item_data <- item_data |>
-      group_by(item) |>
-      arrange(desc(timestamp),
-              .by_group = TRUE) |>
-      mutate(rel_day = row_number() - 1,
-             date = Sys.Date() - rel_day,
-             .after = "price") |>
-      ungroup() |>
-      select(- c("timestamp"))
+      dplyr::group_by(item) |>
+      dplyr::arrange(desc(timestamp),
+                     .by_group = TRUE) |>
+      dplyr::mutate(rel_day = dplyr::row_number() - 1,
+                    date = Sys.Date() - rel_day,
+                    .after = "price") |>
+      dplyr::ungroup() |>
+      dplyr::select(- c("timestamp"))
   }
 
   return(item_data)
