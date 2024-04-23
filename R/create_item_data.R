d7fff33a7afaffb2d1186e26006a8de4f0f638e6
mathiaslj <Math964b@yahoo.dk>
2024-04-23 21:41:02 +0200

    feat: Made changes to create_item_data to be used instead of osrs_api

--- R/create_item_data.R
+++ R/create_item_data.R
@@ -1,31 +1,40 @@
-#' Create a tibble of information about the object
+#' Create a tibble of information with prices of requested item
 #'
-#' @param .x An object returned from [osrs_api()]
+#' @description
+#' The function is a wrapper of [osrs_api()] that returns a tibble which is easier to
+#' work with than the list object from the `osrs_api` function.
+#'
+#' @param items `character` vector of the items
+#' @param history `character` with type of request. Default is `latest`, but `all` is
+#' also available
+#' @param user_agent `character` of the user agent
 #'
 #' @return A tibble with information about items, prices, dates
 #' @export
 #'
 #' @examples
-#' # Get the entire price history of the abyssal whip
-#' whip <- osrs_api(items = "Abyssal_whip", history = "all")
-#'
 #' # Create a data set with information about the item
-#' create_item_data(whip)
+#' create_item_data(items = "Abyssal_whip", history = "all")
 #'
 #' # Same can be done for other values of history
-#' osrs_api(items = c("bucket", "Abyssal whip"), history = "latest") |>
-#'   create_item_data()
-#' osrs_api(items = c("Dragon dagger"), history = "sample") |>
-#'   create_item_data()
+#' create_item_data(items = c("bucket", "Abyssal whip"), history = "latest") |>
+#' create_item_data(items = c("Dragon dagger"), history = "sample")
 #'
-create_item_data <- function(.x) {
-  contents <- .x$content
+create_item_data <- function(items, history = "all", user_agent = "GE_price_tracker") {
+  requests <- purrr::map(items,
+                         osrs_api,
+                         history = history,
+                         user_agent = user_agent)
 
-  item_data <- lapply(contents, function(x) dplyr::bind_rows(x)) |>
-    dplyr::bind_rows(.id = "item")
+  item_data <- purrr::map(requests,
+                          \(x) bind_rows(x$content) |>
+                            dplyr::mutate(item = names(x$content), .before = everything())) |>
+    bind_rows()
 
   # If we have not gotten just a sample, give the relative day of the price
-  if (!grepl("sample", .x$path)) {
+  # Note we are just using the first entry of requests, as it is the same for different
+  # items if we have more
+  if (!grepl("sample", requests[[1]]$path)) {
     item_data <- item_data |>
       dplyr::group_by(item) |>
       dplyr::arrange(desc(timestamp),
--- tests/testthat/test-create_item_data.R
+++ tests/testthat/test-create_item_data.R
@@ -1,26 +1,19 @@
 test_that("create_item_data works with 2 items", {
-  expect_type(create_item_data(osrs_api("Abyssal whip|bucket",
-                                     history = "latest")),
+  expect_type(create_item_data("Abyssal whip|bucket",
+                                   history = "latest"),
               "list")
 })
 
-test_that("create_item_data works with 2 items", {
-  expect_error(create_item_data(osrs_api("Abyssal whip|bucket",
-                                     history = "all")))
+test_that("create_item_data has created a column 'date' if the history argument is not sample", {
+  expect_named(create_item_data("Abyssal whip",
+                                history = "all") |>
+                 dplyr::select(tidyselect::any_of("date")),
+               "date")
 })
 
-test_that("create_item_data has created a column 'date' if the history argument is not sample", {
-            expect_named(osrs_api("Abyssal whip",
-                                  history = "all") |>
-                           create_item_data() |>
-                           dplyr::select(tidyselect::any_of("date")),
-                         "date")
-          })
-
 test_that("create_item_data can still run but without a 'date' column if history argument is sample", {
-  expect_length(osrs_api("Abyssal whip",
-                        history = "sample") |>
-                 create_item_data() |>
-                 dplyr::select(tidyselect::any_of("date")),
-               0)
+  expect_length(create_item_data("Abyssal whip",
+                                 history = "sample") |>
+                  dplyr::select(tidyselect::any_of("date")),
+                0)
 })
